c1d7bef46281686e5a4bb718e90cda5ee75b1d39
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-08-04 11:05:54 +0900

    functionality

--- server.js
+++ server.js
@@ -1,24 +1,89 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
-apiRoutes = require("./routes");
-const PORT = process.env.PORT || 3001;
-require("dotenv").config();
+
+app.use(logger("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.use(express.static("public"));
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", { useNewUrlParser: true });
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([{
+        $addFields: {
+            totalDuration: { $sum: "$exercises.duration" }
+        }
+    }
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.json(err);
+        })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([{
+        $addFields: {
+            totalDuration: { $sum: "$exercises.duration" }
+        }
+    }
+    ])
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+app.get("/", (req, res) => {
+    res.json(path.join(__dirname, "./public/index.html"));
 });
 
-app.use(apiRoutes);
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
+})
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerdb", { useNewUrlParser: true });
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
+})
+
+
 
 app.listen(PORT, () => console.log(`The server has started on PORT: ${PORT}`));
